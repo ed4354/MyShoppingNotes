fa7744709f500b5a803c5155eb21e2638a28f64f
ed4354 <eddli.sac@gmail.com>
2019-02-15 10:32:40 -0800

    Gradient background for MainActivity

--- app/src/main/java/owl/example/myshoppingnotes/MainActivity.java
+++ app/src/main/java/owl/example/myshoppingnotes/MainActivity.java
@@ -1,5 +1,7 @@
 package owl.example.myshoppingnotes;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable)constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
     }
 }
